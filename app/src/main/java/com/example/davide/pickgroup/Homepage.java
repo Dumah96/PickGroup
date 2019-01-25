35c076a058cfc0d69d0e6af1d9d8ed57125224d5
Davide <senatoredavide96@gmail.com>
2019-01-25 12:48:33 +0100

    Funzione aggiunta lingua

--- app/src/main/java/com/example/davide/pickgroup/Homepage.java
+++ app/src/main/java/com/example/davide/pickgroup/Homepage.java
@@ -15,10 +15,16 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -30,6 +36,9 @@ public class Homepage extends AppCompatActivity {
 
 
     private android.app.Fragment fragment = null;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private EditText txtInput;
     private ExpandableListView expListView;
     private HashMap<String, List<String>> listDataChild;
     private ExpandableListAdapter listAdapter;
@@ -48,8 +57,28 @@ public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage2);
+
+        ListView listView = (ListView) findViewById(R.id.listv);
+
+        String[] items = {"Italiano"};
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(this,R.layout.list_lingue,R.id.txtlistalingue,arrayList);
+        listView.setAdapter(adapter);
+        txtInput = (EditText) findViewById(R.id.etlingua);
+        Button btAdd = (Button) findViewById(R.id.btnlingua);
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem = txtInput.getText().toString();
+                arrayList.add(newItem);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.closse);
 
